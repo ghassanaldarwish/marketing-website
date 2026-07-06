a0863f4342c972e158e6f3cc654f5fb7d8aef131
Ghassan <ghassanaldarwish@yahoo.com>
2026-07-06 11:57:34 +0200

    layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,11 @@
-import { Geist, Geist_Mono, Inter } from "next/font/google"
-
-import "./globals.css"
+import {  Geist_Mono, Inter } from "next/font/google"
+import "../styles/globals.css"
 import { ThemeProvider } from "@/components/theme-provider"
-import { cn } from "@/lib/utils";
+import { cn } from "@/lib/utils"
+import Navbar from "@/components/navbar/Navbar"
+import Footer from "@/components/footer/Footer"
 
-const inter = Inter({subsets:['latin'],variable:'--font-sans'})
+const inter = Inter({ subsets: ["latin"], variable: "--font-sans" })
 
 const fontMono = Geist_Mono({
   subsets: ["latin"],
@@ -20,10 +21,21 @@ export default function RootLayout({
     <html
       lang="en"
       suppressHydrationWarning
-      className={cn("antialiased", fontMono.variable, "font-sans", inter.variable)}
+      className={cn(
+        "antialiased",
+        fontMono.variable,
+        "font-sans",
+        inter.variable
+      )}
     >
       <body>
-        <ThemeProvider>{children}</ThemeProvider>
+        <ThemeProvider>
+          <div className="grid grid-rows-[56px_1fr_auto] gap-4 min-h-screen">
+          <Navbar />
+          {children}
+          <Footer />
+          </div>
+        </ThemeProvider>
       </body>
     </html>
   )
