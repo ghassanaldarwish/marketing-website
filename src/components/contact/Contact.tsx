e755ac063ece0d1e57ce315b55916637a4a9041e
Ghassan <ghassanaldarwish@yahoo.com>
2026-07-09 18:14:23 +0200

    layout opt

--- src/components/contact/Contact.tsx
+++ src/components/contact/Contact.tsx
@@ -4,7 +4,7 @@ import * as React from "react"
 import { Controller, useForm } from "react-hook-form"
 import { zodResolver } from "@hookform/resolvers/zod"
 import { toast } from "sonner"
-import * as z from "zod"
+import * as z from "zod/v4"
 import { useTranslations } from "next-intl"
 
 import {
