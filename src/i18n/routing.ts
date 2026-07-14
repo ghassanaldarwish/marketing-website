7205e89068da058fb29095f1c7838bdebcd8b409
Ghassan Aldarwish <ghassanaldarwish@gmail.com>
2026-07-14 17:29:42 +0200

    refactor(i18n): derive routing from published locales [ARC-005]

--- src/i18n/routing.ts
+++ src/i18n/routing.ts
@@ -1,20 +1,13 @@
-// i18n/routing.ts
-import { defineRouting } from "next-intl/routing"
 import { createNavigation } from "next-intl/navigation"
-import { LOCALE } from "@/lib/types"
+import { defineRouting } from "next-intl/routing"
+
+import { defaultLocale, publishedLocales } from "@/i18n/locale"
 
 export const routing = defineRouting({
-  // A list of all locales that are supported
-  locales: Object.values(LOCALE),
-
-  // Used when no locale matches
-  defaultLocale: LOCALE.en,
-
-  // The prefix for the locale in the URL
+  locales: publishedLocales,
+  defaultLocale,
   localePrefix: "always",
 })
 
-// Lightweight wrappers around Next.js' navigation APIs
-// that will consider the routing configuration
 export const { Link, redirect, usePathname, useRouter, getPathname } =
   createNavigation(routing)
